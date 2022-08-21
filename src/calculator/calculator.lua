46740eabb4b454010fb77c67c9b74f4e08aa6c72
Lijon Fogel <lijon.fogel@gmail.com>
2022-08-21 23:50:15 +0200

    best way of do switch case/short circuit if

--- src/calculator/calculator.lua
+++ src/calculator/calculator.lua
@@ -38,11 +38,6 @@ function Calculator:lookupOperation(operation, num1, num2)
         ["/"] = function (n1, n2) return self:div(n1, n2) end
     }
     return (t[operation] and {t[operation](num1, num2)} or {"Invalid operation"})[1]
-    -- if t[operation] then
-    --     return t[operation](num1, num2)
-    -- else
-    --     return "Invalid operation"
-    -- end
 end
 
 return Calculator
